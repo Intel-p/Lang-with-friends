059d5896b629549f371a4da445d36af05aae56ff
Intel-p <89892667+Intel-p@users.noreply.github.com>
2021-08-31 23:13:54 -0500

    Update source.cpp

--- source.cpp
+++ source.cpp
@@ -1,7 +1,29 @@
+//OPLang founded by William Shepelak and Liam Donavin 8/31/2021
+
+//import statments
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
 using namespace std();
+
+//function prototyping
+vector<string> tokenizer(string);
+string file_to_string(fname);
+
 
 int main()
 {
 
 }
+
+vector<string> tokenizer(string str)
+{
+
+}
+
+string file_to_string(fname)
+{
+
+}
+

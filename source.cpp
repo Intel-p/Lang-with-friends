25424b9938e6ba017f0cffe9714b45f45cac49bd
WILLIAM SHEPELAK <89892667+Intel-p@users.noreply.github.com>
2021-09-01 15:38:46 +0000

    fixed compile command and made early version of file_to_string method

--- source.cpp
+++ source.cpp
@@ -10,12 +10,14 @@ using namespace std;
 //function prototyping
 /*
 vector<string> tokenizer(string);
-string file_to_string(string);
 */
+string file_to_string(string);
+
 
 int main()
 {
-	cout << "Hello world\n";
+	string code = file_to_string("code.txt");
+	cout << code << endl;
 	cin.get();
 	return 0;
 }
@@ -25,9 +27,18 @@ vector<string> tokenizer(string str)
 {
 
 }
+*/
 
 string file_to_string(string fname)
 {
-
+	fstream _file;
+	_file.open(fname.c_str());
+	string str, rtrn_str;
+	while(getline(_file, str))
+	{
+		rtrn_str+=str + "\n";
+	}
+	_file.close();
+	return rtrn_str;
 }
-*/
+

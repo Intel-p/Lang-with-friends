c662025c5bd610724973b16f14086aaa3e215f02
WILLIAM SHEPELAK <89892667+Intel-p@users.noreply.github.com>
2021-09-01 04:29:47 +0000

    Fixed compiling bugs

--- source.cpp
+++ source.cpp
@@ -5,25 +5,29 @@
 #include <fstream>
 #include <string>
 #include <vector>
-using namespace std();
+using namespace std;
 
 //function prototyping
+/*
 vector<string> tokenizer(string);
-string file_to_string(fname);
-
+string file_to_string(string);
+*/
 
 int main()
 {
 	cout << "Hello world\n";
+	cin.get();
+	return 0;
 }
 
+/*
 vector<string> tokenizer(string str)
 {
 
 }
 
-string file_to_string(fname)
+string file_to_string(string fname)
 {
 
 }
-
+*/

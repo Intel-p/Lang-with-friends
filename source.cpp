2b7e748dd9a9aeb2d6ba6e6f590ac933dffc8bc6
WILLIAM SHEPELAK <89892667+Intel-p@users.noreply.github.com>
2021-09-02 15:50:29 +0000

    created tokenizer method and updated file_to_string

--- source.cpp
+++ source.cpp
@@ -8,37 +8,54 @@
 using namespace std;
 
 //function prototyping
-/*
-vector<string> tokenizer(string);
-*/
+vector<string> tokenizer(string, string);
 string file_to_string(string);
 
 
 int main()
 {
 	string code = file_to_string("code.txt");
-	cout << code << endl;
+	vector<string> tokens = tokenizer(code, " ");
+	for(auto i=tokens.begin();i!=tokens.end();i++)
+	{
+		cout << *i << endl;
+	}
 	cin.get();
 	return 0;
 }
 
-/*
-vector<string> tokenizer(string str)
+vector<string> tokenizer(string str, string delimitor)
 {
-
+	vector<string> rtrn_vector;
+	size_t pos;
+	while((pos=str.find(delimitor)) != string::npos)
+	{
+		rtrn_vector.push_back(str.substr(0,pos));
+		str.erase(0,pos+delimitor.size());
+	}
+	rtrn_vector.push_back(str);
+	return rtrn_vector;
 }
-*/
 
 string file_to_string(string fname)
 {
 	fstream _file;
+	string rtrn_str;
 	_file.open(fname.c_str());
-	string str, rtrn_str;
-	while(getline(_file, str))
+	if(!_file.fail())
 	{
-		rtrn_str+=str + "\n";
+		string str;
+		while(getline(_file, str))
+		{
+			rtrn_str+=str+"\n";
+		}
+	}
+	else{
+		rtrn_str = "";
+		cout << "The program file does not exist!" << endl;
 	}
 	_file.close();
 	return rtrn_str;
 }
+
 
